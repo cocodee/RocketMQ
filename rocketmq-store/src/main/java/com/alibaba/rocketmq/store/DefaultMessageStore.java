02936b58251d9675bcafcddae7881668d1050bb7
cocodee <coco.dee@gmail.com>
2015-01-26 14:30:29 +0800

    Read
    Read

--- rocketmq-store/src/main/java/com/alibaba/rocketmq/store/DefaultMessageStore.java
+++ rocketmq-store/src/main/java/com/alibaba/rocketmq/store/DefaultMessageStore.java
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * Read by kdi
  */
 package com.alibaba.rocketmq.store;
 
